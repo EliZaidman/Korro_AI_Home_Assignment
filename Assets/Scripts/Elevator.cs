6fa179f402c6e822ae5b1b6c4cd203b9a1115038
EliZaidman <zaidmaneli@gmail.com>
2024-01-09 18:29:00 +0200

    Platform Variants + elevator

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class Elevator : MonoBehaviour
 {
     private GameObject player;
+    public float topY; 
+    private float bottomY; 
+    public float speed = 1.0f; // Speed of the elevator
 
-    private void OnTriggerEnter(Collider other)
+    private bool goingUp = true; 
+
+    private void Start()
     {
-        if (other.gameObject.CompareTag("Player"))
+        bottomY = transform.position.y;
+    }
+    void Update()
+    {
+        float targetY = goingUp ? topY : bottomY;
+
+        transform.position = new Vector3(transform.position.x,
+                                         Mathf.Lerp(transform.position.y, targetY, Time.deltaTime),
+                                         transform.position.z);
+
+        if (goingUp && transform.position.y >= topY - 0.5f)
         {
-            player = other.gameObject;
+            goingUp = false; 
+        }
+        else if (!goingUp && transform.position.y <= bottomY + 0.5f)
+        {
+            goingUp = true; 
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject;
             player.transform.parent = transform;
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnCollisionExit(Collision collision)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             player.transform.parent = null;
         }
